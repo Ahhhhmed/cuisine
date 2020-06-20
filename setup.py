ba2c4d5a3384d8dd3a82137a0d66786bf042776b
Nenad Vasic <ahhhhmed@gmail.com>
2020-06-20 19:32:40 +0200

    fix syntax error

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author_email="Ahhhhmed@gmail.com",
     description="A bunch of recepies.",
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
     url="https://github.com/ahhhhmed/peper",
     packages=setuptools.find_packages(),
     classifiers=[
