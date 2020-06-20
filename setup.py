6e7d30c7e15f6b559cea4fc77763568adc58f330
Nenad Vasic <ahhhhmed@gmail.com>
2020-06-20 21:46:52 +0200

    0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="peper",
-    version="0.0.7",
+    version="0.0.8",
     author="Nenad",
     author_email="Ahhhhmed@gmail.com",
     description="A bunch of recepies.",
